b037cc1c926ec86de1ad959f5f9a753d2ddefb48
camclark02 <164952709+camclark02@users.noreply.github.com>
2024-05-06 01:07:31 +0000

    most of it working

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,4 +13,26 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
+  def title_with_year
+    # return self.title + "(" + self.year.to_s + ")"
+
+    return "#{self.title} (#{self.year})"
+  end
+ 
+  def director
+    d_id = self.director_id
+
+    matching_directors = Director.where({ :id => d_id })
+
+    director = matching_directors.at(0)
+    return director
+  end
+
+  def characters
+    my_id = self.id
+
+    matching_characters = Character.where({ :movie_id => my_id })
+    return matching_characters
+  end
+
 end
